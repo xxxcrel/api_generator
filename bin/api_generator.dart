14c188e4ce5caef21ef840e89fb1d5cdc7ebf876
ShiinaManatsu <whitesky@live.cn>
2020-12-05 16:56:09 +0800

    support optional save path

--- bin/api_generator.dart
+++ bin/api_generator.dart
@@ -2,6 +2,8 @@ library api_generator;
 
 import 'dart:convert';
 import 'dart:io';
+import 'package:api_generator/src/path_not_exist.dart';
+
 import '../lib/src/swager_to_dart_api.dart';
 import '../lib/src/swaget_to_dart_json.dart';
 
@@ -9,6 +11,7 @@ var encoding = Encoding.getByName("utf-8");
 
 String template =
     "/// \$Summary\$ static Future<\$ReturnType\$> \$GeneratedApiName\$(\$ApiParameters\$) async => await httpPost<\$ReturnType\$>(\"\$ApiUrl\$\", \$ApiParametersMap\$, converter: (data) => \$ReturnType\$.fromJson(data));";
+String _defaultSavePath = "./lib";
 
 void main() {
   String url, savePath;
@@ -18,10 +21,29 @@ void main() {
     stdout.write("Please enter api url:");
     url = stdin.readLineSync(encoding: encoding);
   }
-  print("The api url: $url");
-  stdout.write("please enter save path(default: lib/api/):");
-  print("default template: $template");
-  savePath = "./";
+  // print("The api url: $url");
+  while (true) {
+    stdout.write("Please enter save path(default:lib/api):");
+    savePath = stdin.readLineSync(encoding: encoding);
+    if (savePath.isEmpty || savePath.length == 0) {
+      savePath = _defaultSavePath;
+    }
+    if(pathExist(savePath)){
+      break;
+    }else{
+      print("can't find path \"$savePath\", please create first");
+    }
+  }
+  print("default template:\n $template");
+  
   swagerToDartJson(url, savePath);
   swagerToDartApi(url, template, savePath);
+}
+
+bool pathExist(String path) {
+  var file = Directory(path);
+  if (!file.existsSync()) {
+    return false;
+  }
+  return true;
 }
